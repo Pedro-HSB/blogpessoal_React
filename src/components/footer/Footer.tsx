12f3abef46fbf466d3f85b4f168548097be43ab8
pedro-hsb <pedro.bernardo4561@gmail.com>
2024-01-30 12:06:15 -0300

    criacao de alerts personalizados e page/perfil

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -15,9 +15,9 @@ function Footer() {
                     <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
+                        <a href=""><LinkedinLogo size={48} weight='bold' /></a>
+                        <a href=""><InstagramLogo size={48} weight='bold' /></a>
+                        <a href=""><FacebookLogo size={48} weight='bold' /></a>
                     </div>
                 </div>
             </div>
