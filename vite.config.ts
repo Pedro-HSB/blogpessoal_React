2721117c4a5924746d6c936805933ee497b46160
pedro-hsb <pedro.bernardo4561@gmail.com>
2024-01-30 12:21:42 -0300

    Vite dev server option  can be bypassed when hosted on case-insensitive filesystem

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// export default defineConfig({
+
+// })
+
+export default {
   plugins: [react()],
-})
+  server:
+  {
+    fs:
+    {
+      deny:
+        ['.env', '.env.*', '*.{crt,pem}', 'custom.secret']
+    }
+  }
+}
