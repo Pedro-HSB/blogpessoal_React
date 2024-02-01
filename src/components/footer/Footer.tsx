96cee932b2a126317ee71252cdf5a01c1f809c71
pedro-hsb <pedro.bernardo4561@gmail.com>
2024-02-01 15:05:23 -0300

    Deploy do Projeto Projeto Integrador

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -12,7 +12,7 @@ function Footer() {
         FooterComponent = (
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: </p>
+                    <p className='text-xl font-bold'>Blog pessoal Pedro | Copyright: </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
                         <a href=""><LinkedinLogo size={48} weight='bold' /></a>
